c6761bf504ca8ac5bb6885449f37183381ace58f
inql <dbinkus96@gmail.com>
2018-11-20 12:56:21 +0100

    Implemented room class

--- include/Room.h
+++ include/Room.h
@@ -8,6 +8,28 @@
 #include "Main.h"
 
 class Room {
+public:
+    Room();
+    ~Room();
+
+    void setRoomId(Uint8 roomId);
+    void setHostId(Uint8 hostId);
+    void setMapId(Uint8 mapId);
+
+    void addClientToRoom(Client* client);
+
+    Uint8 getRoomId();
+    Uint8 getHostId();
+    Uint8 getMapId();
+
+    Client* getClient(Uint8 id);
+private:
+
+    std::vector<std::unique_ptr<Client>> clients;
+
+    Uint8 roomId;
+    Uint8 hostId;
+    Uint8 mapId;
 
 };
 
--- src/Room.cpp
+++ src/Room.cpp
@@ -2,4 +2,49 @@
 // Created by inql on 20.11.18.
 //
 
+#include <Room.h>
+
 #include "Main.h"
+
+Room::Room() = default;
+
+Room::~Room() = default;
+
+void Room::setRoomId(Uint8 roomId) {
+    this->roomId = roomId;
+}
+
+void Room::setHostId(Uint8 hostId) {
+    this->hostId = hostId;
+}
+
+void Room::setMapId(Uint8 mapId) {
+    this->mapId = mapId;
+}
+
+void Room::addClientToRoom(Client *client) {
+    clients.push_back(std::unique_ptr<Client>(client));
+}
+
+Uint8 Room::getRoomId() {
+    return this->roomId;
+}
+
+Uint8 Room::getHostId() {
+    return this->hostId;
+}
+
+Uint8 Room::getMapId() {
+    return this->mapId;
+}
+
+Client *Room::getClient(Uint8 id) {
+    for(auto& client: clients)
+    {
+        if(client->getId()==id){
+            return client.get();
+        }
+    }
+    return nullptr;
+}
+
