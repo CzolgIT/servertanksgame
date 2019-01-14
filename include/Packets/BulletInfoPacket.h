1509650b40af43f5e6d21f8fb6ed8e24c3794a0b
inql <dbinkus96@gmail.com>
2019-01-14 15:35:41 +0100

    extend bullet info packet

--- include/Packets/BulletInfoPacket.h
+++ include/Packets/BulletInfoPacket.h
@@ -6,7 +6,8 @@
 #define SERVERTANKSGAME_BULLETINFOPACKET_H
 
 #include "Main.h"
-#define BULLET_INFO_PACKET_SIZE (1+1+1+2+2+2)
+//typ+playerid+bulletid+x+y+angle+speed
+#define BULLET_INFO_PACKET_SIZE (1+1+1+2+2+2+1)
 class BulletInfoPacket : public BasePacket{
 public:
     BulletInfoPacket();
@@ -15,12 +16,14 @@ public:
     void setX(Uint16 x);
     void setY(Uint16 y);
     void setAngle(Uint16 angle);
+    void setSpeed(unsigned int speed);
 
     Uint8 getPlayerId() const;
     Uint8 getBulletId() const;
     Uint16 getX() const;
     Uint16 getY() const;
     Uint16 getAngle() const;
+    unsigned int getSpeed() const;
 
     void print() const override;
 };
