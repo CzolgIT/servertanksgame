694814fc3c31c6aeeedf7514e7a775217e467cca
Matmalecki <mmateusz314@gmail.com>
2019-01-20 12:57:43 +0100

    Fix

--- src/Engine/EngineManager.cpp
+++ src/Engine/EngineManager.cpp
@@ -58,6 +58,7 @@ void EngineManager::checkColliders()
             if (col.x != 0 || col.y != 0)
             {
                 client->doDamage( 10 );
+                if (client->getActHp() < 1)
                 for(auto& cli: *clients)
                 {
                     if(cli->getId()==bullet->getClientId()){
